1aae38a7691211dc3f3aace932ab141ffed377b6
Hugo Escalpelo <hugoescalpelo@gmail.com>
2023-11-29 19:10:07 -0600

    Update record-mp3.py

--- python/test/record-mp3.py
+++ python/test/record-mp3.py
@@ -5,11 +5,19 @@ import numpy as np
 import io
 
 # Configuración
-button_pin = 2  # Cambia esto al número de pin que estés usando
+button_pin = 4  # Cambia esto al número de pin que estés usando
 fs = 44100  # Frecuencia de muestreo
+gain = 0.8  # Ajuste de ganancia (menor que 1 disminuye, mayor que 1 aumenta)
+bit_depth = 16  # Profundidad de bits (16 o 24 usualmente)
 
 # Inicializar botón
 button = Button(button_pin)
+
+# Función para ajustar la profundidad de bits
+def adjust_bit_depth(audio_data, target_depth):
+    max_val = 2**(target_depth - 1) - 1
+    scaled_data = audio_data / np.max(np.abs(audio_data)) * max_val
+    return scaled_data.astype(np.int16) if target_depth == 16 else scaled_data.astype(np.int32)
 
 # Función para grabar audio
 def record_audio():
@@ -19,17 +27,22 @@ def record_audio():
         audio_data = []
         while button.is_pressed:
             data, _ = stream.read(fs)
+            # Aplicar ajuste de ganancia
+            data = data * gain
             audio_data.append(data)
         audio_data = np.concatenate(audio_data, axis=0)
+        # Ajustar profundidad de bits
+        audio_data = adjust_bit_depth(audio_data, bit_depth)
     print("Grabación finalizada")
     return audio_data
 
 # Función para guardar el audio en formato MP3
 def save_audio(audio_data, filename="grabacion.mp3"):
+    sample_width = 2 if bit_depth == 16 else 3  # 2 bytes para 16 bits, 3 para 24 bits
     audio = AudioSegment(
         audio_data.tobytes(),
         frame_rate=fs,
-        sample_width=audio_data.dtype.itemsize,
+        sample_width=sample_width,
         channels=1
     )
     audio.export(filename, format="mp3")
@@ -41,3 +54,4 @@ while True:
     button.wait_for_press()
     audio_data = record_audio()
     save_audio(audio_data)
+
