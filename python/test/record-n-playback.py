b364d4f724056ae7721a1dd94219a39de48b67f8
Hugo Escalpelo <hugoescalpelo@gmail.com>
2023-11-29 19:30:53 -0600

    Create record-n-playback.py

---
+++ python/test/record-n-playback.py
@@ -0,0 +1,84 @@
+from gpiozero import Button
+import sounddevice as sd
+from pydub import AudioSegment
+import numpy as np
+import io
+import datetime
+import random
+import threading
+import os
+import time
+
+# Configuración
+button_pin = 4  # Cambia esto al número de pin que estés usando
+fs = 44100  # Frecuencia de muestreo
+gain = 0.8  # Ajuste de ganancia
+bit_depth = 16  # Profundidad de bits
+recordings_folder = "recordings"  # Carpeta para guardar las grabaciones
+os.makedirs(recordings_folder, exist_ok=True)
+
+# Inicializar botón
+button = Button(button_pin)
+
+# Función para ajustar la profundidad de bits
+def adjust_bit_depth(audio_data, target_depth):
+    max_val = 2**(target_depth - 1) - 1
+    scaled_data = audio_data / np.max(np.abs(audio_data)) * max_val
+    return scaled_data.astype(np.int16) if target_depth == 16 else scaled_data.astype(np.int32)
+
+# Función para grabar audio
+def record_audio():
+    print("Comenzando grabación...")
+    with sd.InputStream(samplerate=fs, channels=1) as stream:
+        audio_data = []
+        while button.is_pressed:
+            data, _ = stream.read(fs)
+            data = data * gain
+            audio_data.append(data)
+        audio_data = np.concatenate(audio_data, axis=0)
+        audio_data = adjust_bit_depth(audio_data, bit_depth)
+    print("Grabación finalizada")
+    return audio_data
+
+# Función para generar el nombre del archivo
+def generate_filename():
+    now = datetime.datetime.now()
+    return os.path.join(recordings_folder, now.strftime("%Y-%m-%d-%H-%M-%S.mp3"))
+
+# Función para guardar el audio en formato MP3
+def save_audio(audio_data, filename):
+    sample_width = 2 if bit_depth == 16 else 3
+    audio = AudioSegment(
+        audio_data.tobytes(),
+        frame_rate=fs,
+        sample_width=sample_width,
+        channels=1
+    )
+    audio.export(filename, format="mp3")
+    print(f"Audio guardado como {filename}")
+
+# Función para reproducir audios de forma aleatoria
+def play_random_recordings():
+    while True:
+        try:
+            files = os.listdir(recordings_folder)
+            if files:
+                filename = random.choice(files)
+                audio = AudioSegment.from_file(os.path.join(recordings_folder, filename))
+                playback = audio._spawn(audio.raw_data)
+                playback.start()
+                time.sleep(len(audio) / 1000.0)
+        except Exception as e:
+            print(f"Error al reproducir: {e}")
+
+# Hilo para reproducir grabaciones
+playback_thread = threading.Thread(target=play_random_recordings, daemon=True)
+playback_thread.start()
+
+# Loop principal
+print("Presione el botón para grabar.")
+while True:
+    button.wait_for_press()
+    audio_data = record_audio()
+    filename = generate_filename()
+    save_audio(audio_data, filename)
